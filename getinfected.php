48b09360bb063e78b00f9c5829ea039e52f84f58
harrylongworth <harry@harrylongworth.com>
2015-07-17 09:46:13 +1000

    Update getinfected.php

--- getinfected.php
+++ getinfected.php
@@ -651,7 +651,7 @@
 
             // current test stub instead of admin page opens in new window:
             // echo '<h2>Infection Complete!</h2><p>Check infection has worked: </p><p><a href="admin" target="_blank">Click Here for Admin Page</a></p><p>or</p><p><a href="play" target="_blank">Click Here for PLAY Page</a></p>'; $_SESSION['isValidation']['flag'] = FALSE;
-            echo '<h2>Infection Complete!</h2><p><a href="admin" target="_blank">Next . . </a></p>'; $_SESSION['isValidation']['flag'] = FALSE;
+            echo '<h2>Infection Complete!</h2><h2><a href="admin" target="_blank"> Next . . </a></h2>'; $_SESSION['isValidation']['flag'] = FALSE;
             $installed=1;
         }
     }
