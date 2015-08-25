4b0d6bcebec20c11809597c185355d9d6273956d
Vishal Simon <vishalsimon20121981@gmail.com>
2015-08-25 08:17:24 +0530

    Set admin url

--- getinfected.php
+++ getinfected.php
@@ -658,7 +658,7 @@
 
                 // current test stub instead of admin page opens in new window:
                 // echo '<h2>Infection Complete!</h2><p>Check infection has worked: </p><p><a href="admin" target="_blank">Click Here for Admin Page</a></p><p>or</p><p><a href="play" target="_blank">Click Here for PLAY Page</a></p>'; $_SESSION['isValidation']['flag'] = FALSE;
-                echo '<h2>Infection Complete!</h2><h2><a href="admin"> Next . . </a></h2>'; $_SESSION['isValidation']['flag'] = FALSE;
+                echo '<h2>Infection Complete!</h2><h2><a href="'.SITE_URL.'/admin"> Next . . </a></h2>'; $_SESSION['isValidation']['flag'] = FALSE;
                 if(file_exists($sDestination))
                 {
                     require_once($sDestination);
@@ -947,7 +947,7 @@
 
                 // current test stub instead of admin page opens in new window:
                 // echo '<h2>Infection Complete!</h2><p>Check infection has worked: </p><p><a href="admin" target="_blank">Click Here for Admin Page</a></p><p>or</p><p><a href="play" target="_blank">Click Here for PLAY Page</a></p>'; $_SESSION['isValidation']['flag'] = FALSE;
-                echo '<h2>Infection Complete!</h2><h2><a href="admin"> Next . . </a></h2>'; $_SESSION['isValidation']['flag'] = FALSE;
+                echo '<h2>Infection Complete!</h2><h2><a href="'.SITE_URL.'/admin"> Next . . </a></h2>'; $_SESSION['isValidation']['flag'] = FALSE;
                 if(file_exists($sDestination))
                 {
                     require_once($sDestination);
